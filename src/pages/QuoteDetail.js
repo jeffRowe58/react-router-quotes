4f57884dec846a2aff3ab71e2655a4ab77879c84
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-11-29 16:46:25 -0600

    monday update

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,16 @@
-import {useParams} from "react-router-dom";
+import {Route, useParams} from "react-router-dom";
 import {Fragment} from "react";
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
     const params = useParams();
     return (
         <Fragment>
             <h1>Quote Detail Page</h1>
-            <p>{params.qouteId}</p>
+            <p>{params.quoteId}</p>
+            <Route path={`/quotes/${params.quoteId}/comments`}>
+                <Comments />
+            </Route>
         </Fragment>
     )
 };
